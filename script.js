3e8f861bda1e2fccda32a922f34f09ce6c579178
ils <usworkpost@yandex.ru>
2025-01-11 21:14:56 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const city = "izh" //какой город
+const temper = "+1" // какая температура
+
+console.log ("city ", city)
+console.log ("temp ", temper)
+
+const result = (1 + 2) * 5; // result выводит сумма умноженную на множитель
+console.log (result)
